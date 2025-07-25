aa6dfdff479e7064316badb0d279b10dbde97dd0
Balint Kesmarki <balint.kesmarki@gmail.com>
2025-07-25 20:03:37 +0200

    rework in progress

---
+++ Main/frontend/js/board.js
@@ -0,0 +1,44 @@
+import * as util from "./utils.js";
+import * as constant from "./constants.js";
+import { visualize } from "./visualize.js";
+
+class chessboard{
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+        this.board = [];
+        this.board_no_highlight = [];
+
+    };
+
+    init(positions){
+        for(let w = 0; w < width; w++){
+            this.board.push([]);
+            for(let h = 0; h < height; h++){
+                this.board[w].push(0);
+            };
+        };
+    }
+
+    value(pos){
+        return(this.board[pos[0]][pos[1]]);
+    };
+
+    highlightPos(positions){
+        this.board_no_highlight = JSON.parse(util.strfy(this.board));
+
+        for(let i = 0; i < positions.length; i++){
+            this.board[positions[i][0]][positions[i][1]] = "■";
+        };
+        visualize();
+    };
+
+    get clear(){
+        this.board = JSON.parse(util.strfy(this.board_no_highlight));
+        visualize();
+    };
+};
+
+let board = new chessboard(constant.board_width, constant.board_height);
+
+export { board };
